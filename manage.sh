e50c70ccc5c1f6fe31b42dd255011a419e4262b0
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-07-13 12:01:18 -0400

    allow multiple commands

--- manage.sh
+++ manage.sh
@@ -1,89 +1,100 @@
 #!/bin/bash
 
+BUILD_DIR=./build
+
 if [ "$#" -lt 2 ]; then
     echo "Usage: `basename $0` [build|shell|clean|deploy] [tool1] [tool2] .. [tooln]"
+    echo "       You can combine multiple commands with a plus, e.g. clean+build+deploy"
     exit 1
 fi
 
-COMMAND=$1
-BUILD_DIR=./build
+COMMANDS=$1
+CMDARR=(${COMMANDS//+/ })
+shift 1 # remove command parameter from args
 
-if [ "$COMMAND" == "build" ]; then
+for COMMAND in "${CMDARR[@]}"
+do
+    echo "Executing $COMMAND command on these targets: $@"
 
-    mkdir -p $BUILD_DIR
+    if [ "$COMMAND" == "build" ]; then
 
-    shift 1 # remove command parameter from args
-    echo "Will build these images: $@"
+        mkdir -p $BUILD_DIR
 
-    for ALIGNER in "$@"
-    do
-        VERSION=`grep VERSION $ALIGNER/Singularity | sed "s/VERSION //"`
-        FILENAME=${ALIGNER}-${VERSION}.img
-        sudo rm /tmp/$FILENAME && true
-        echo "---------------------------------------------------------------------------------"
-        echo " Building image for $ALIGNER"
-        echo "---------------------------------------------------------------------------------"
-        pushd $ALIGNER
-        sudo singularity build /tmp/$FILENAME Singularity
-        popd
-        FINAL=$BUILD_DIR/$FILENAME
-        cp /tmp/$FILENAME $FINAL
-        sudo rm /tmp/$FILENAME
-        echo "Created container $FINAL"
-    done
+        echo "Will build these images: $@"
 
-elif [ "$COMMAND" == "shell" ]; then
+        for ALIGNER in "$@"
+        do
+            ALIGNER=${ALIGNER%/}
+            VERSION=`grep VERSION $ALIGNER/Singularity | sed "s/VERSION //"`
+            FILENAME=${ALIGNER}-${VERSION}.img
+            sudo rm /tmp/$FILENAME && true
+            echo "---------------------------------------------------------------------------------"
+            echo " Building image for $ALIGNER"
+            echo "---------------------------------------------------------------------------------"
+            pushd $ALIGNER
+            sudo singularity build /tmp/$FILENAME Singularity
+            popd
+            FINAL=$BUILD_DIR/$FILENAME
+            cp /tmp/$FILENAME $FINAL
+            sudo rm /tmp/$FILENAME
+            echo "Created container $FINAL"
+        done
 
-    ALIGNER=$2
-    VERSION=`grep VERSION $ALIGNER/Singularity | sed "s/VERSION //"`
-    FILENAME=${ALIGNER}-${VERSION}.img
-    IMGFILE=$BUILD_DIR/$FILENAME
+    elif [ "$COMMAND" == "shell" ]; then
 
-    if [ ! -f $IMGFILE ]; then
-        echo "Container $IMGFILE not found. You must first build the aligner container with build.sh"
-        exit 1
-    fi
-
-    singularity shell $IMGFILE
-
-elif [ "$COMMAND" == "deploy" ]; then
-
-    if [ -z "$JACS_SINGULARITY_DIR" ]; then
-        echo "Set the JACS_SINGULARITY_DIR environment variable to the directory where containers will be deployed"
-        exit 1
-    fi
-
-    shift 1 # remove command parameter from args
-    echo "Will deploy these images: $@"
-
-    for ALIGNER in "$@"
-    do
+        ALIGNER=${2%/}
         VERSION=`grep VERSION $ALIGNER/Singularity | sed "s/VERSION //"`
         FILENAME=${ALIGNER}-${VERSION}.img
         IMGFILE=$BUILD_DIR/$FILENAME
 
         if [ ! -f $IMGFILE ]; then
             echo "Container $IMGFILE not found. You must first build the aligner container with build.sh"
-        else
-            echo "Copying $FILENAME to $JACS_SINGULARITY_DIR"
-            cp $IMGFILE $JACS_SINGULARITY_DIR
+            exit 1
         fi
 
-    done
+        singularity shell $IMGFILE
 
-elif [ "$COMMAND" == "clean" ]; then
+    elif [ "$COMMAND" == "deploy" ]; then
 
-    shift 1 # remove command parameter from args
-    echo "Will clean these images: $@"
+        if [ -z "$JACS_SINGULARITY_DIR" ]; then
+            echo "Set the JACS_SINGULARITY_DIR environment variable to the directory where containers will be deployed"
+            exit 1
+        fi
 
-    for ALIGNER in "$@"
-    do
-        # I hope ALIGNER doesn't have any spaces in it!
-        rm -f $BUILD_DIR/${ALIGNER}*
-    done
+        shift 1 # remove command parameter from args
+        echo "Will deploy these images: $@"
 
-else
-    echo "Unknown command: $COMMAND"
-    exit 1
-fi
+        for ALIGNER in "$@"
+        do
+            ALIGNER=${ALIGNER%/}
+            VERSION=`grep VERSION $ALIGNER/Singularity | sed "s/VERSION //"`
+            FILENAME=${ALIGNER}-${VERSION}.img
+            IMGFILE=$BUILD_DIR/$FILENAME
 
+            if [ ! -f $IMGFILE ]; then
+                echo "Container $IMGFILE not found. You must first build the aligner container with build.sh"
+            else
+                echo "Copying $FILENAME to $JACS_SINGULARITY_DIR"
+                cp $IMGFILE $JACS_SINGULARITY_DIR
+            fi
+
+        done
+
+    elif [ "$COMMAND" == "clean" ]; then
+
+        shift 1 # remove command parameter from args
+        echo "Will clean these images: $@"
+
+        for ALIGNER in "$@"
+        do
+            ALIGNER=${ALIGNER%/}
+            # I hope ALIGNER doesn't have any spaces in it!
+            rm -f $BUILD_DIR/${ALIGNER}*
+        done
+
+    else
+        echo "Unknown command: $COMMAND"
+        exit 1
+    fi
+
+done
