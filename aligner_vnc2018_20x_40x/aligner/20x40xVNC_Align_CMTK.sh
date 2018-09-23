e532339108fb947ffc3c9cada587df052b36f938
Konrad Rokicki <rokickik@janelia.hhmi.org>
2018-09-23 17:14:26 -0400

    better logging for 12bit conversion

--- aligner_vnc2018_20x_40x/aligner/20x40xVNC_Align_CMTK.sh
+++ aligner_vnc2018_20x_40x/aligner/20x40xVNC_Align_CMTK.sh
@@ -500,7 +500,12 @@ else
     echo "cmtk_warping stop: $STOP"
 fi
 
-$FIJI -macro $TWELVEBITCONV "$OUTPUT/,"$filename"_01.nrrd,$gloval_nc82_nrrd"
+echo " "
+echo "+----------------------------------------------------------------------+"
+echo "| 12-bit conversion"
+echo "| $FIJI -macro $TWELVEBITCONV \"$OUTPUT/,\"$filename\"_01.nrrd,$gloval_nc82_nrrd\""
+echo "+----------------------------------------------------------------------+"
+$FIJI -headless -macro $TWELVEBITCONV "$OUTPUT/,"$filename"_01.nrrd,$gloval_nc82_nrrd" > $OUTPUT/conv12bit.log 2>&1
 
 ########################################################################################################
 # JRC2018 gender-specific reformat
