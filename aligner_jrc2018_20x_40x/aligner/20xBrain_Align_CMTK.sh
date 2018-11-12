0fdc9a999abeb9ccb8e3998d06599633ee0be0e1
HideoOtsuna <otsuna@muc.biglobe.ne.jp>
2018-11-12 15:27:07 -0500

    gen1 unisex fix

--- aligner_jrc2018_20x_40x/aligner/20xBrain_Align_CMTK.sh
+++ aligner_jrc2018_20x_40x/aligner/20xBrain_Align_CMTK.sh
@@ -521,7 +521,7 @@ fi
 if [[ $TRESOLUTION == "20x_gen1" ]]; then
     voxel_size="0.6214809x0.6214809x1.0"
     voxel_size_uni="0.5189161x0.5189161x1.0"
-    image_size_uni="1010x473x174"
+    image_size_uni="1210x566x174"
 
     if [[ $INPUT1_GENDER == "f" ]]; then
         image_size="1010x470x182"
