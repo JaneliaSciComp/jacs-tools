65e383306757717e3d599d2dbf980fb0277c75f4
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-10-07 18:34:39 -0400

    updated docker version

--- deploy_dockers.sh
+++ deploy_dockers.sh
@@ -13,8 +13,8 @@ echo "Deploying to $JACS_SINGULARITY_DIR"
 #singularity build -F /tmp/out.sif docker-daemon://janeliascicomp/flylight_tools:1.0.1
 #cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_tools-1.0.1.img
 
-singularity build -F /tmp/out.sif docker://registry.int.janelia.org/jacs-scripts/flylight_perl:1.0.0
-cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_perl-1.0.0.img
+singularity build -F /tmp/out.sif docker://registry.int.janelia.org/jacs-scripts/flylight_perl:1.0.3
+cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_perl-1.0.3.img
 
 #singularity build -F /tmp/out.sif docker://janeliascicomp/neusep_flylight:1.0.0
 #cp /tmp/out.sif $JACS_SINGULARITY_DIR/neusep_flylight-1.0.0.img
