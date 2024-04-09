b66f57c7409778dfbd8307dcdc219f0ccb62276d
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-04-09 09:56:33 -0400

    updated version

--- deploy_dockers.sh
+++ deploy_dockers.sh
@@ -10,16 +10,16 @@ echo "Deploying to $JACS_SINGULARITY_DIR"
 #singularity build -F /tmp/out.sif docker://janeliascicomp/vaa3d:redhat-mark13
 #cp /tmp/out.sif $JACS_SINGULARITY_DIR/vaa3d-redhat-mark13.img
 
-#V=1.3.0
-#singularity build -F /tmp/out.sif docker-daemon://janeliascicomp/flylight_tools:$V
-#cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_tools-$V.img
+V=1.4.0
+singularity build -F /tmp/out.sif docker-daemon://janeliascicomp/flylight_tools:$V
+cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_tools-$V.img
 
 #singularity build -F /tmp/out.sif docker://registry.int.janelia.org/jacs/jacs-storage:1.19.2
 #cp /tmp/out.sif $JACS_SINGULARITY_DIR/jacs-storage-1.19.2.img
 
-V=1.0.5
-singularity build -F /tmp/out.sif docker://registry.int.janelia.org/jacs-scripts/flylight_perl:1.0.5
-cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_perl-1.0.5.img
+#V=1.0.5
+#singularity build -F /tmp/out.sif docker://registry.int.janelia.org/jacs-scripts/flylight_perl:1.0.5
+#cp /tmp/out.sif $JACS_SINGULARITY_DIR/flylight_perl-1.0.5.img
 
 #singularity build -F /tmp/out.sif docker://janeliascicomp/neusep_flylight:1.0.0
 #cp /tmp/out.sif $JACS_SINGULARITY_DIR/neusep_flylight-1.0.0.img
